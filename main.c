a548ec52da057ce050516c80348049e6d54391c1
Celthans <celthans.dezan@etu.u-paris.fr>
2024-09-29 23:54:12 +0200

    C1 à finir (existe etudiant?)

--- main.c
+++ main.c
@@ -1,17 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #pragma warning(disable:4996)
 
-#include "inscription .h"
+//#include "inscription .h"
+enum{LONGUEUR_MAX=30, LIMITE=100 };
+
+typedef struct{
+    char nom_etu[LONGUEUR_MAX];
+    int no_groupe;
+    int id;
+}Etudiants ;
+
+Etudiants etudiants[LIMITE];
 
 
-    int main(){
+void inscription(char*nom_etu, int no_groupe ) {
+    Etudiants e;
 
-        inscription(nom_etu,no_groupe);
+    int nb_etudiants=0;
 
 
-       exit(EXIT_FAILURE); //---------------C0-----------------------
-
+    if (nb_etudiants < LIMITE) {
+        if (!existe_etudiant(e.nom_etu, e.no_groupe)) {
+            etudiants[LIMITE].id = nb_etudiants + 1;
+            strcpy(etudiants[nb_etudiants].nom_etu, e.nom_etu);
+            etudiants[nb_etudiants].no_groupe = e.no_groupe;
+            nb_etudiants++;
+            printf("Inscription enregistree (%d)\n", nb_etudiants);
         }
+        else  {
+            printf("Nom incorrect\n");
+        }
+    }
+    else {
+        printf("Erreur : nombre maximum d'étudiants atteint\n");
+    }
+}
+
+
+int main(){
+        Etudiants e;
+    inscription(e.nom_etu, e.no_groupe);
+
+
+
+        exit(EXIT_FAILURE);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
