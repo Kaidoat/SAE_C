b7acd1bc3f49bd383590fe5bc5f8e17a4339e1ee
Celthans <153358552+Kaidoat@users.noreply.github.com>
2024-10-01 16:55:32 +0200

    Update main.c
    C1 a opti

--- main.c
+++ main.c
@@ -3,63 +3,89 @@
 #include <string.h>
 #include <stdbool.h>
 #pragma warning(disable:4996)
+#define LIMITE 100
+#define LONGUEUR_MAX 30
 
 //#include "inscription .h"
-enum{LONGUEUR_MAX=30, LIMITE=100 };
+
 
 typedef struct{
     char nom_etu[LONGUEUR_MAX];
     int no_groupe;
     int id;
-}Etudiants ;
+}Etudiant ;
 
-Etudiants etudiants[LIMITE];
+Etudiant etudiants[LIMITE];
 
 
-
-void inscription(char*nom_etu, int no_groupe) {
-
-    int nb_etudiants=0;
-
-    while (nb_etudiants < LIMITE) {
-
-        etudiants[LIMITE].id =  nb_etudiants+1;
-        scanf("%s %d", etudiants[LIMITE].nom_etu, etudiants[LIMITE].no_groupe);
-        strcpy(etudiants[nb_etudiants].nom_etu, nom_etu);
-        etudiants[nb_etudiants].no_groupe = no_groupe;
-        nb_etudiants++;
-        printf("Inscription enregistree (%d)\n", nb_etudiants);
+int verifier_nom(Etudiant etudiants[], int nb_etudiants, char nom_etu[], int no_groupe) {
+    for (int i = 0; i < nb_etudiants; i++) {
+        if (strcmp(etudiants[i].nom_etu, nom_etu) == 0 && etudiants[i].no_groupe == no_groupe) {
+            return 1; // Nom déjà utilisé dans ce groupe
+        }
     }
+    return 0;
+}
 
+void inscription(){
+    Etudiant e;
+    Etudiant etudiants[LIMITE];
+    int nb_etudiants = 0;
+    char nom_etu[LONGUEUR_MAX];
+    int no_groupe;
+    char max= 100;
+    char input[max];
+
+
+        while (nb_etudiants < LIMITE) {
+            scanf("%s %d", nom_etu, &no_groupe);
+
+
+            // Vérification de la longueur du nom
+            if (strlen(nom_etu) > LONGUEUR_MAX - 1) {
+                printf("Nom incorrect\n");
+                continue;
+            }
+
+            // Vérification si le nom existe déjà dans le groupe
+            if (verifier_nom(etudiants, nb_etudiants, nom_etu, no_groupe)) {
+                printf("Nom incorrect\n");
+                continue;
+            }
+
+
+            // Enregistrement de l'étudiant
+            strcpy(etudiants[nb_etudiants].nom_etu, nom_etu);
+            etudiants[nb_etudiants].no_groupe = no_groupe;
+            nb_etudiants++;
+
+            // Affichage de l'inscription enregistrée avec l'identifiant
+            printf("Inscription enregistree (%d)\n", nb_etudiants);
+
+}
 }
 
 
-//     if (strcmp)  {
-//         printf("Nom incorrect\n");
-//     }
-//
-// if (nb_etudiants ==LIMITE) {
-//     printf("Erreur : nombre maximum d'étudiants atteint\n");
-// }
-// }
+
 //----------C1------------//
 
 
 
 int main(){
-    Etudiants e;
-    char input[LONGUEUR_MAX];
+    Etudiant e;
+    int max=100;
+    char input[max];
 
 do{
     scanf("%s",input);
-    if (strcmp(input, "inscription")!=0){ //C1
-        inscription("Lea", 101);
+    if (strcmp(input, "inscription")==0){ //----------C1---------//
+        inscription();
     }
 
 
 
 
-}while(strcmp(input, "exit")!=0); //C0
+}while(strcmp(input, "exit")!=0); //--------C0-----------//
 
 
 
