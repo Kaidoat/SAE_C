3663661f3bae6ed9ae6dc70bb4a4593ab64d072e
Celthans <celthans.dezan@etu.u-paris.fr>
2024-10-15 00:20:11 +0200

    var gloc regle

--- Brouillon_C1.c
+++ Brouillon_C1.c
@@ -1,59 +1,214 @@
 //#include <stdio.h>
 //#include <stdlib.h>
 //#include <string.h>
+//#include <stdbool.h>
+//#pragma warning(disable:4996)
 //
-////struct etudiant
-//typedef struct {
-//    int id;
-//    char nom[30];
-//    int groupe;
-//    int absences;
-//} Etudiant;
+//#define MAX_INPUT 150
+//#define LONGUEUR_MAX_COMMANDE 20
+//#define LIMITE 100
+//#define LONGUEUR_MAX_ETUDIANT 30
+//#define JOUR_MAX 40
+//#define LEN_AM_PM 3
+//#define EXCUSE_MAX 80
+//#define LONGUEUR_MAX_JUSTIF 50
 //
-////struct absence
+//#define AM 0
+//#define PM 1
+//
+//typedef enum {
+//    A_FOURNIR = 0,
+//    A_VALIDER,
+//    ACCEPTEE,
+//    REFUSEE
+//} StatutExcuse;
+//
 //typedef struct {
-//    int id;
-//    int id_etudiant;
-//    int jour;
-//    char demi_journee[3];
-//    char justificatif[51];
-//    int valide;
+//    unsigned int jour;
+//    int demi_journee;
+//    char excuse[EXCUSE_MAX];
+//    StatutExcuse statut_excuse;
 //} Absence;
 //
-//Etudiant etudiants[100];
-//
-//Absence absences[100];
-//
-//int nb_etudiants = 0;
-//
-//int nb_absences = 0;
-//
-// //void inscription(char*nom_etu, int no_groupe ) {
-//
-//     int nb_etudiants=0;
+//typedef struct {
+//    char nom_etu[LONGUEUR_MAX_ETUDIANT + 1]; // +1 pour zero final
+//    unsigned int no_groupe;
+//    unsigned int nb_absences;
+//    Absence absences[JOUR_MAX * 2];
+//} Etudiant;
 //
 //
-//     if (nb_etudiants < LIMITE) {
-//         etudiants[LIMITE].id = nb_etudiants + 1;
-//         strcpy(etudiants[nb_etudiants].nom_etu, nom_etu);
-//         etudiants[nb_etudiants].no_groupe = no_groupe;
-//         printf("Inscription enregistree (%d)\n", nb_etudiants);
-//         nb_etudiants++;
 //
-//     }
-//     // if (strcmp(nom_etu, no_groupe)) {
-//     //     etudiants[LIMITE].id = nb_etudiants + 1;
-//     //     strcpy(etudiants[nb_etudiants].nom_etu, nom_etu);
-//     //     etudiants[nb_etudiants].no_groupe = no_groupe;
-//     //     nb_etudiants++;
-//     //     printf("Inscription enregistree (%d)\n", nb_etudiants);
-//     // }
-//     else  {
-//         printf("Nom incorrect\n");
-//     }
-// }
-// //     else {
-// //         printf("Erreur : nombre maximum d'étudiants atteint\n");
-// //     }
-// // }
-//// existe étduiant a faire ensuite
+////---------------C1--------------//
+//
+//int verifier_nom(Etudiant etudiants[], int nb_etudiants, char* nom_etu, int no_groupe) {
+//    for (int i = 0; i < nb_etudiants; i++) {
+//        if (strcmp(etudiants[i].nom_etu, nom_etu) == 0 && etudiants[i].no_groupe == no_groupe) {
+//            return 1; // Nom déjà utilisé dans ce groupe
+//        }
+//    }
+//    return 0;
+//}
+//
+//void inscription(Etudiant etudiants[], int* nb_etudiants) {
+//    Etudiant e;
+//    char nom_etu[MAX_INPUT];
+//    int no_groupe;
+//
+//    scanf("%s %d", nom_etu, &no_groupe);
+//
+//    if (*nb_etudiants < LIMITE) {
+//        // Vérification de la longueur du nom
+//        if (strlen(nom_etu) > LONGUEUR_MAX_ETUDIANT) {
+//            printf("Nom incorrect\n");
+//            return;
+//        }
+//
+//        // Vérification si le nom existe déjà dans le groupe
+//        if (verifier_nom(etudiants, *nb_etudiants, nom_etu, no_groupe)) {
+//            printf("Nom incorrect\n");
+//            return;
+//        }
+//
+//        // Enregistrement de l'étudiant en derniere position dans le tableau
+//        strcpy(etudiants[*nb_etudiants].nom_etu, nom_etu);
+//        etudiants[*nb_etudiants].no_groupe = no_groupe;
+//
+//        // augmente le nb d'etudiants inscrits
+//        (*nb_etudiants)++;
+//
+//        // Affichage de l'inscription enregistrée avec l'identifiant
+//        printf("Inscription enregistree (%d)\n", *nb_etudiants);
+//    } else {
+//        printf("Nb max d'etudiants atteint\n");
+//    }
+//}
+//
+//
+////-------------------C2----------------//
+//void absence(Etudiant etudiants[], int nb_etudiants) {
+//    Absence a;
+//    char nom_etu[LONGUEUR_MAX_ETUDIANT];
+//    int no_etudiant = 0;
+//    a.jour = 0;
+//    char am_pm[LEN_AM_PM];
+//    int demi_journee;
+//
+//    scanf("%d %d %s", &no_etudiant, &a.jour, am_pm);
+//    // transforme le num etudiant en index du tableau
+//    no_etudiant--;
+//
+//    if (a.jour < 1 || a.jour > JOUR_MAX) {
+//        printf("Jour incorrect\n");
+//        return;
+//    }
+//
+//    if (no_etudiant >= nb_etudiants || no_etudiant < 0) {
+//        printf("Identifiant incorrect\n");
+//        return;
+//    }
+//
+//    if (strcmp(am_pm, "am") == 0) {
+//        demi_journee = AM;
+//    } else if (strcmp(am_pm, "pm") == 0) {
+//        demi_journee = PM;
+//    } else {
+//        printf("Commande inconnue\n");
+//        return;
+//    }
+//
+//    etudiants[no_etudiant].absences[etudiants[no_etudiant].nb_absences].jour = a.jour;
+//    etudiants[no_etudiant].absences[etudiants[no_etudiant].nb_absences].demi_journee = demi_journee;
+//    etudiants[no_etudiant].absences[etudiants[no_etudiant].nb_absences].excuse[0] = 0;
+//    etudiants[no_etudiant].absences[etudiants[no_etudiant].nb_absences].statut_excuse = A_FOURNIR;
+//
+//    etudiants[no_etudiant].nb_absences++;
+//    printf("Absence enregistree [%d]\n", etudiants[no_etudiant].nb_absences);
+//}
+//
+//
+//
+////----------------------C3--------------//
+//void liste_etudiants(Etudiant etudiants[], int nb_etudiants) {
+//    if (nb_etudiants == 0) {
+//        printf("Aucun inscrit\n");
+//        return;
+//    }
+//
+//    for (int i = 0; i < nb_etudiants; ++i) {
+//        // Utiliser i + 1 pour avoir les indices dans le bon ordre
+//        printf("(%d) %s %d %d\n", nb_etudiants - i, etudiants[nb_etudiants - 1 - i].nom_etu, etudiants[nb_etudiants - 1 - i].no_groupe, etudiants[nb_etudiants - 1 - i].nb_absences);
+//    }
+//}
+//
+//
+//
+////--------------C4-----------//
+//void depot_justificatif(Etudiant etudiants[], int nb_etudiants) {
+//    int no_etudiant, jour;
+//    char justificatif[LONGUEUR_MAX_JUSTIF];  // pour le texte de l'excuse
+//
+//    scanf("%d %d %[^\n]", &no_etudiant, &jour, justificatif);
+//
+//    if (no_etudiant > nb_etudiants || no_etudiant <= 0) {
+//        printf("Identifiant incorrect\n");
+//        return;
+//    }
+//
+//    Etudiant* etudiant = &etudiants[no_etudiant - 1];
+//
+//    // Recherche de l'absence correspondant au jour
+//    int index_absence = -1;
+//    for (int i = 0; i < etudiant->nb_absences; i++) {
+//        if (etudiant->absences[i].jour == jour) {
+//            index_absence = i;
+//            break;
+//        }
+//    }
+//
+//    if (index_absence == -1) {
+//        printf("Absence non trouvée\n");
+//        return;
+//    }
+//
+//    Absence* absence = &etudiant->absences[index_absence];
+//
+//    // Cas où le justificatif est déjà fourni
+//    if (absence->statut_excuse == ACCEPTEE) {
+//        printf("Justificatif deja connu\n");
+//        return;
+//    }
+//
+//    // Vérification si justificatif est fourni dans les temps (3j max)
+//    if (jour <= absence->jour + 3) {
+//        strcpy(absence->excuse, justificatif);
+//        absence->statut_excuse = A_VALIDER;
+//        printf("Justificatif enregistre\n");
+//    } else {
+//        strcpy(absence->excuse, justificatif);
+//        absence->statut_excuse = REFUSEE;
+//        printf("Justificatif enregistre\n");
+//    }
+//}
+//
+////----------Main------------//
+//int main() {
+//    Etudiant etudiants[LIMITE];
+//    int nb_etudiants = 0;
+//    char input[LONGUEUR_MAX_COMMANDE];
+//
+//    do {
+//        scanf("%s", input);
+//        if (strcmp(input, "inscription") == 0) { //----------C1---------//
+//            inscription(etudiants, &nb_etudiants);
+//        } else if (strcmp(input, "absence") == 0) { //----------C2---------//
+//            absence(etudiants, nb_etudiants);
+//        } else if (strcmp(input, "etudiants") == 0) { //----------C3---------//
+//            liste_etudiants(etudiants, nb_etudiants);
+//        } else if (strcmp(input, "justificatif") == 0) { //---------C4----------//
+//            depot_justificatif(etudiants, nb_etudiants);
+//        } else if (strcmp(input, "exit") == 0) {
+//            break;
+//        }
+//    } while (strcmp(input, "exit") != 0); //--------C0-----------//
+//}
--- main.c
+++ main.c
@@ -23,24 +23,21 @@ typedef enum {
     REFUSEE
 } StatutExcuse;
 
-
 typedef struct {
     unsigned int jour;
     int demi_journee;
     char excuse[EXCUSE_MAX];
     StatutExcuse statut_excuse;
-}Absence;
+} Absence;
 
 typedef struct {
-    char nom_etu[LONGUEUR_MAX_ETUDIANT+1]; // +1 pour zero final
+    char nom_etu[LONGUEUR_MAX_ETUDIANT + 1]; // +1 pour zero final
     unsigned int no_groupe;
     unsigned int nb_absences;
     Absence absences[JOUR_MAX * 2];
-}Etudiant;
+} Etudiant;
 
 
-Etudiant etudiants[LIMITE];
-int nb_etudiants = 0;
 
 //---------------C1--------------//
 
@@ -53,14 +50,14 @@ int verifier_nom(Etudiant etudiants[], int nb_etudiants, char* nom_etu, int no_g
     return 0;
 }
 
-void inscription() {
+void inscription(Etudiant etudiants[], int* nb_etudiants) {
     Etudiant e;
     char nom_etu[MAX_INPUT];
     int no_groupe;
 
     scanf("%s %d", nom_etu, &no_groupe);
 
-    if (nb_etudiants < LIMITE) {
+    if (*nb_etudiants < LIMITE) {
         // Vérification de la longueur du nom
         if (strlen(nom_etu) > LONGUEUR_MAX_ETUDIANT) {
             printf("Nom incorrect\n");
@@ -68,38 +65,34 @@ void inscription() {
         }
 
         // Vérification si le nom existe déjà dans le groupe
-        if (verifier_nom(etudiants, nb_etudiants, nom_etu, no_groupe)) {
+        if (verifier_nom(etudiants, *nb_etudiants, nom_etu, no_groupe)) {
             printf("Nom incorrect\n");
             return;
         }
 
         // Enregistrement de l'étudiant en derniere position dans le tableau
-        strcpy(etudiants[nb_etudiants].nom_etu, nom_etu);
-        etudiants[nb_etudiants].no_groupe = no_groupe;
+        strcpy(etudiants[*nb_etudiants].nom_etu, nom_etu);
+        etudiants[*nb_etudiants].no_groupe = no_groupe;
 
         // augmente le nb d'etudiants inscrits
-        nb_etudiants++;
+        (*nb_etudiants)++;
 
         // Affichage de l'inscription enregistrée avec l'identifiant
-        printf("Inscription enregistree (%d)\n", nb_etudiants);
-    }
-    else {
+        printf("Inscription enregistree (%d)\n", *nb_etudiants);
+    } else {
         printf("Nb max d'etudiants atteint\n");
     }
 }
 
 
-
-
 //-------------------C2----------------//
-void absence() {
+void absence(Etudiant etudiants[], int nb_etudiants) {
     Absence a;
     char nom_etu[LONGUEUR_MAX_ETUDIANT];
     int no_etudiant = 0;
     a.jour = 0;
     char am_pm[LEN_AM_PM];
     int demi_journee;
-
 
     scanf("%d %d %s", &no_etudiant, &a.jour, am_pm);
     // transforme le num etudiant en index du tableau
@@ -110,12 +103,6 @@ void absence() {
         return;
     }
 
-
-    if (no_etudiant >= nb_etudiants) {
-        printf("Identifiant incorrect\n");
-        return;
-    }
-
     if (no_etudiant >= nb_etudiants || no_etudiant < 0) {
         printf("Identifiant incorrect\n");
         return;
@@ -123,11 +110,9 @@ void absence() {
 
     if (strcmp(am_pm, "am") == 0) {
         demi_journee = AM;
-    }
-    else if (strcmp(am_pm, "pm") == 0) {
+    } else if (strcmp(am_pm, "pm") == 0) {
         demi_journee = PM;
-    }
-    else {
+    } else {
         printf("Commande inconnue\n");
         return;
     }
@@ -136,53 +121,30 @@ void absence() {
     etudiants[no_etudiant].absences[etudiants[no_etudiant].nb_absences].demi_journee = demi_journee;
     etudiants[no_etudiant].absences[etudiants[no_etudiant].nb_absences].excuse[0] = 0;
     etudiants[no_etudiant].absences[etudiants[no_etudiant].nb_absences].statut_excuse = A_FOURNIR;
-    
+
     etudiants[no_etudiant].nb_absences++;
     printf("Absence enregistree [%d]\n", etudiants[no_etudiant].nb_absences);
-
 }
-
-int compare_etudiants(Etudiant* E1, Etudiant* E2) {
-    if (E1->no_groupe != E2->no_groupe)
-        return E1->no_groupe - E2->no_groupe;
-    else
-        return strcmp(E1->nom_etu, E2->nom_etu);
-}
-
-
-
-
-
 
 
 
 //----------------------C3--------------//
-void liste_etudiants() {
-
-    Absence a;
-    a.jour=0;
-    scanf("%d",&a.jour);
-
-
-    if (a.jour<1){
-        printf("Date incorrecte\n");
-
-    }
-
-    if (nb_etudiants==0){
+void liste_etudiants(Etudiant etudiants[], int nb_etudiants) {
+    if (nb_etudiants == 0) {
         printf("Aucun inscrit\n");
-
+        return;
     }
 
-    for (int i = 0; i < nb_etudiants ; ++i) {
+    for (int i = 0; i < nb_etudiants; ++i) {
         // Utiliser i + 1 pour avoir les indices dans le bon ordre
-        printf("(%d) %s %d %d\n", nb_etudiants-i, etudiants[nb_etudiants - 1 - i].nom_etu, etudiants[nb_etudiants - 1 - i].no_groupe, etudiants[nb_etudiants - 1 - i].nb_absences);
+        printf("(%d) %s %d %d\n", nb_etudiants - i, etudiants[nb_etudiants - 1 - i].nom_etu, etudiants[nb_etudiants - 1 - i].no_groupe, etudiants[nb_etudiants - 1 - i].nb_absences);
     }
 }
 
 
+
 //--------------C4-----------//
-void depot_justificatif() {
+void depot_justificatif(Etudiant etudiants[], int nb_etudiants) {
     int no_etudiant, jour;
     char justificatif[LONGUEUR_MAX_JUSTIF];  // pour le texte de l'excuse
 
@@ -193,10 +155,10 @@ void depot_justificatif() {
         return;
     }
 
-    Etudiant* etudiant = &etudiant[no_etudiant - 1];
+    Etudiant* etudiant = &etudiants[no_etudiant - 1];
 
     // Recherche de l'absence correspondant au jour
-    int index_absence = 0;
+    int index_absence = -1;
     for (int i = 0; i < etudiant->nb_absences; i++) {
         if (etudiant->absences[i].jour == jour) {
             index_absence = i;
@@ -204,60 +166,49 @@ void depot_justificatif() {
         }
     }
 
+    if (index_absence == -1) {
+        printf("Absence non trouvée\n");
+        return;
+    }
+
     Absence* absence = &etudiant->absences[index_absence];
 
-
-    // cas ou le justif est deja fourni
+    // Cas où le justificatif est déjà fourni
     if (absence->statut_excuse == ACCEPTEE) {
         printf("Justificatif deja connu\n");
         return;
     }
 
-
-    // Verification si justificatif est fourni dans les temps (3j max)
+    // Vérification si justificatif est fourni dans les temps (3j max)
     if (jour <= absence->jour + 3) {
         strcpy(absence->excuse, justificatif);
         absence->statut_excuse = A_VALIDER;
         printf("Justificatif enregistre\n");
-    }
-    else {
+    } else {
         strcpy(absence->excuse, justificatif);
         absence->statut_excuse = REFUSEE;
         printf("Justificatif enregistre\n");
     }
-
 }
 
-
-
-
 //----------Main------------//
-
 int main() {
+    Etudiant etudiants[LIMITE];
+    int nb_etudiants = 0;
     char input[LONGUEUR_MAX_COMMANDE];
+
     do {
         scanf("%s", input);
         if (strcmp(input, "inscription") == 0) { //----------C1---------//
-            inscription();
-        }
-        else if (strcmp(input, "absence") == 0) { //----------C2---------//
-            absence();
-        }
-
-        else if (strcmp(input, "etudiants") == 0) { //----------C3---------//
-            liste_etudiants();
-        }
-
-        else if (strcmp(input, "justificatif") ==0){ //---------C4----------//
-            depot_justificatif();
-        }
-
-        else if (strcmp(input, "exit") == 0) {
+            inscription(etudiants, &nb_etudiants);
+        } else if (strcmp(input, "absence") == 0) { //----------C2---------//
+            absence(etudiants, nb_etudiants);
+        } else if (strcmp(input, "etudiants") == 0) { //----------C3---------//
+            liste_etudiants(etudiants, nb_etudiants);
+        } else if (strcmp(input, "justificatif") == 0) { //---------C4----------//
+            depot_justificatif(etudiants, nb_etudiants);
+        } else if (strcmp(input, "exit") == 0) {
             break;
         }
-
     } while (strcmp(input, "exit") != 0); //--------C0-----------//
 }
-
-
-
